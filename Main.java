319dd0fe9c1d762057cfe073966b9b943f756778
Malek BenKhalifa <benkhalifamalek29@gmail.com>
2023-10-03 19:54:29 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal lion = new Animal();
+        lion.age = 5;
+        lion.family = "cats";
+        System.out.println(lion.name);
+        Animal dog = new Animal("canine","snoopy",5,true);
+        System.out.println(dog.name);
+        Zoo myZoo= new Zoo("wildlife","Ariana",25);
+
+        System.out.println(myZoo);
+       /* myZoo.displayZoo();
+
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+        */
+        System.out.println(dog);
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+        System.out.println(myZoo.searchAnimal(dog));
+
+        Animal chat = new Animal("chat", "chat", 6, true);
+        System.out.println(myZoo.searchAnimal(dog));
+        boolean removed = myZoo.removeAnimal(lion);
+        System.out.println(removed);
+
+
+
+
+
+
+
+    }
+}
